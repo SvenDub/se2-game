0e4beca57331f0e59b223c8febb68592a102fbe1
Sven Dubbeld <s.dubbeld@student.fontys.nl>
2016-03-16 23:04:34 +0100

    Initial test project

---
+++ Tests/UnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ontwikkelopdracht_Game;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+            
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            
+        }
+    }
+}
