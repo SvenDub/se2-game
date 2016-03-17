a7573f1801f2fe9ea7cd462a68ce8e08f3ccd997
Sven Dubbeld <s.dubbeld@student.fontys.nl>
2016-03-17 19:42:13 +0100

    Add MoveableObject tests

--- Tests/Entity/MoveableObjectTests.cs
+++ Tests/Entity/MoveableObjectTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ontwikkelopdracht_Game.Entity;
 
 namespace Tests.Entity
 {
@@ -7,8 +8,29 @@ namespace Tests.Entity
     public class MoveableObjectTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestRotate()
         {
+            Player player = new Player
+            {
+                Rotation = 0
+            };
+
+            player.Rotate(Math.PI);
+
+            Assert.AreEqual(Math.PI, player.Rotation);
+        }
+
+        [TestMethod]
+        public void TestMultipleRotations()
+        {
+            Player player = new Player
+            {
+                Rotation = 0
+            };
+
+            player.Rotate(Math.PI*3.5);
+
+            Assert.AreEqual(Math.PI*1.5, player.Rotation);
         }
     }
 }
