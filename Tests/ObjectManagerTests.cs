47a289fb350f2cc90d52ffa5714a3ba06b2445b4
Sven Dubbeld <s.dubbeld@student.fontys.nl>
2016-03-17 12:59:38 +0100

    Add GameObject tests

--- Tests/ObjectManagerTests.cs
+++ Tests/ObjectManagerTests.cs
@@ -9,6 +9,8 @@ namespace Tests
     [TestClass]
     public class ObjectManagerTests
     {
+        private readonly ObjectManager _objectManager = ObjectManager.Instance;
+
         [TestInitialize]
         public void SetUp()
         {
@@ -18,25 +20,25 @@ namespace Tests
         [TestCleanup]
         public void TearDown()
         {
-            ObjectManager.Instance.GameObjects.Clear();
+            _objectManager.GameObjects.Clear();
         }
 
         [TestMethod]
         public void TestAdding()
         {
             GameObject testObject = new Wall();
-            ObjectManager.Instance.AddObject(testObject);
-            Assert.IsTrue(ObjectManager.Instance.GameObjects.Count == 1);
+            _objectManager.AddObject(testObject);
+            Assert.AreEqual(1, _objectManager.GameObjects.Count);
         }
 
         [TestMethod]
         public void TestDeleting()
         {
             GameObject testObject = new Wall();
-            ObjectManager.Instance.AddObject(testObject);
-            Assert.IsTrue(ObjectManager.Instance.GameObjects.Count == 1);
-            ObjectManager.Instance.RemoveObject(testObject);
-            Assert.IsTrue(ObjectManager.Instance.GameObjects.Count == 0);
+            _objectManager.AddObject(testObject);
+            Assert.AreEqual(1, _objectManager.GameObjects.Count);
+            _objectManager.RemoveObject(testObject);
+            Assert.AreEqual(0, _objectManager.GameObjects.Count);
         }
 
         [TestMethod]
@@ -66,15 +68,61 @@ namespace Tests
                 Height = 10
             };
 
-            ObjectManager.Instance.AddObject(gameObject);
-            ObjectManager.Instance.AddObject(intersectingGameObject);
-            ObjectManager.Instance.AddObject(nonIntersectingGameObject);
+            _objectManager.AddObject(gameObject);
+            _objectManager.AddObject(intersectingGameObject);
+            _objectManager.AddObject(nonIntersectingGameObject);
 
-            Assert.IsTrue(ObjectManager.Instance.Intersects(intersectingGameObject));
-            Assert.IsFalse(ObjectManager.Instance.Intersects(nonIntersectingGameObject));
+            Assert.IsTrue(_objectManager.Intersects(intersectingGameObject));
+            Assert.IsFalse(_objectManager.Intersects(nonIntersectingGameObject));
 
-            Assert.IsTrue(ObjectManager.Instance.Intersects(nonIntersectingGameObject, new Rectangle(0, 0, 10, 10)));
-            Assert.IsFalse(ObjectManager.Instance.Intersects(nonIntersectingGameObject, new Rectangle(0, 0, 5, 5), new List<GameObject> {gameObject}));
+            Assert.IsTrue(_objectManager.Intersects(nonIntersectingGameObject, new Rectangle(0, 0, 10, 10)));
+            Assert.IsFalse(_objectManager.Intersects(intersectingGameObject, new List<GameObject> { gameObject }));
+            Assert.IsFalse(_objectManager.Intersects(nonIntersectingGameObject, new Rectangle(0, 0, 5, 5), new List<GameObject> {gameObject}));
+        }
+
+        [TestMethod]
+        public void TestIntersectedObjects()
+        {
+            GameObject gameObject = new Wall
+            {
+                X = 0,
+                Y = 0,
+                Width = 10,
+                Height = 10
+            };
+
+            GameObject intersectingGameObject = new Wall
+            {
+                X = 0,
+                Y = 5,
+                Width = 10,
+                Height = 10
+            };
+
+            GameObject nonIntersectingGameObject = new Wall
+            {
+                X = 10,
+                Y = 0,
+                Width = 10,
+                Height = 10
+            };
+
+            _objectManager.AddObject(gameObject);
+            _objectManager.AddObject(intersectingGameObject);
+            _objectManager.AddObject(nonIntersectingGameObject);
+
+            List<GameObject> intersectedObjects = _objectManager.IntersectedObjects(gameObject);
+            Assert.IsTrue(intersectedObjects.Contains(intersectingGameObject));
+            Assert.IsFalse(intersectedObjects.Contains(nonIntersectingGameObject));
+
+            intersectedObjects = _objectManager.IntersectedObjects(nonIntersectingGameObject, new Rectangle(0, 0, 10, 10));
+            Assert.IsTrue(intersectedObjects.Contains(gameObject));
+
+            intersectedObjects = _objectManager.IntersectedObjects(intersectingGameObject, new List<GameObject> { gameObject });
+            Assert.IsFalse(intersectedObjects.Contains(gameObject));
+
+            intersectedObjects = _objectManager.IntersectedObjects(nonIntersectingGameObject, new Rectangle(0, 0, 5, 5), new List<GameObject> { gameObject });
+            Assert.IsFalse(intersectedObjects.Contains(gameObject));
         }
     }
 }
