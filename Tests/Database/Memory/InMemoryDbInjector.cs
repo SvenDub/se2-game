9467a3334af3d2b189182f30c24d179ab51701f4
Sven Dubbeld <s.dubbeld@student.fontys.nl>
2016-04-01 19:44:44 +0200

    Unit tests

---
+++ Tests/Database/Memory/InMemoryDbInjector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Ontwikkelopdracht_Game.Database;
+using Ontwikkelopdracht_Game.Entity;
+
+namespace Tests.Database.Memory
+{
+    public static class InMemoryDbInjector
+    {
+        public static IDictionary<Type, Type> Types
+        {
+            get
+            {
+                IDictionary<Type, Type> types = new Dictionary<Type, Type>();
+
+                // Repositories
+                types.Add(typeof(IRepository<List<GameObject>, string>), typeof(MapRepository));
+
+                return types;
+            }
+        }
+    }
+}
---
+++ Tests/Database/Memory/MapRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Ontwikkelopdracht_Game;
+using Ontwikkelopdracht_Game.Database;
+using Ontwikkelopdracht_Game.Entity;
+
+namespace Tests.Database.Memory
+{
+    public class MapRepository : IRepository<List<GameObject>, string>
+    {
+        private readonly IDictionary<string, List<GameObject>> _levelsList = new Dictionary<string, List<GameObject>>();
+
+        public MapRepository()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            _levelsList.Clear();
+
+            _levelsList.Add("One", LevelPreset.One);
+            _levelsList.Add("Test", LevelPreset.Test);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public IEnumerable<string> List()
+        {
+            return _levelsList.Keys;
+        }
+
+        public void Delete(string id)
+        {
+            _levelsList.Remove(id);
+        }
+        
+        public bool Exists(string id)
+        {
+            return _levelsList.ContainsKey(id);
+        }
+
+        public List<GameObject> FindOne(string id)
+        {
+            try
+            {
+                return _levelsList[id];
+            }
+            catch (Exception e)
+            {
+                throw new LevelLoadException(e);
+            }
+        }
+
+        public S Save<S>(S entity, string id) where S : List<GameObject>
+        {
+            try
+            {
+                _levelsList[id] = entity;
+
+                return entity;
+            }
+            catch (Exception e)
+            {
+                throw new LevelSaveException(e);
+            }
+        }
+    }
+}
